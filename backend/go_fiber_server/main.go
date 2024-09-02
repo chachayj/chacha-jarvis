1247624bc446b003b7630c5e806bf11af63f91cc
youngjincha <yjcha@integrit.ai>
2024-09-02 17:42:29 +0900

    [CCJ-5] Feat : load .env

--- backend/go_fiber_server/main.go
+++ backend/go_fiber_server/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"log"
+
+	"github.com/joho/godotenv"
+
     "github.com/gofiber/fiber/v2"
 )
 
 func main() {
     // Fiber 앱 생성
     app := fiber.New()
+
+	// .env 파일 읽기
+	env_err := godotenv.Load()
+	if env_err != nil {
+		log.Fatalf("Error loading .env file: %v", env_err)
+	}
 
     // 루트 라우트 설정
     app.Get("/", func(c *fiber.Ctx) error {
