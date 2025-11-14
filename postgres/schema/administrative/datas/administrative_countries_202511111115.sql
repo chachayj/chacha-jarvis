6ed74d89a95637ec829258718540cfb9d2478aa0
yjcha <yjcha@metabuild.co.kr>
2025-11-14 15:19:38 +0900

    [CHACH-5] feat : OSM 테이블 설계

---
+++ postgres/schema/administrative/datas/administrative_countries_202511111115.sql
@@ -0,0 +1,2 @@
+INSERT INTO administrative.administrative_countries (country_code,country_name) VALUES
+	 ('KR','대한민국');
---
+++ postgres/schema/administrative/datas/administrative_level_mapping_202511111115.sql
@@ -0,0 +1,7 @@
+INSERT INTO administrative.administrative_level_mapping (osm_admin_level,iso_un_level_code,iso_un_level_int,description_ko,description_en) VALUES
+	 (2,'0',0,'국가','Country'),
+	 (4,'1',1,'광역지자체(특별시·광역시·도)','High-level local government'),
+	 (6,'2',2,'시·군·자치구','City / County / Autonomous district'),
+	 (8,'3',3,'자치구가 아닌 구·읍·면·동','Non-autonomous district / Town / Township / Neighborhood'),
+	 (9,'3_sub',3,'리','Village (ri)'),
+	 (10,'3_sub2',3,'자연마을·통 등 소구역','Hamlet / small locality');
